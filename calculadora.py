f1fbbc2bbf68a3b9f6fa618671a8257f3ab9df99
fasnfilho <fabinhosnf@gmail.com>
2024-05-06 01:14:26 -0300

    Fazendo a calculadora da missão 1

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+a = int(input("Digite o primeiro numero: "))
+b = int(input("Digite o segundo numero: "))
+
+print("Resultado da adição: " + str(a + b))
+print("Resultado da subtração: " + str(a - b))
+print("Resultado da multiplicação: " + str(a * b))
+print("Resultado da divisão: " + str(a / b))
